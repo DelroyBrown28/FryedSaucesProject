fd107b5a9d34edf922c99480c9a06b165f4cc821
Delroy Brown <Delroy_jb@hotmail.co.uk>
2020-10-24 17:01:44 +0100

    added detail view for product

--- cart/views.py
+++ cart/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.views import generic
+from .models import Product
 
-# Create your views here.
+
+class ProductListView(generic.ListView):
+    template_name = 'cart/product_list.html'
+    queryset = Product.objects.all()
+
+
+class ProductDetailView(generic.DetailView):
+    template_name = 'cart/product_detail.html'
+    queryset = Product.objects.all()
