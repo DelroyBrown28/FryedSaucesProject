96b906d6f872b014ff2009ac66d303e3cb0c9a8d
Delroy Brown <Delroy_jb@hotmail.co.uk>
2020-10-28 09:49:48 +0000

    moved to new repo and changed products

--- FryedSauces/wsgi.py
+++ FryedSauces/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for RubyMoonProjectMain project.
+WSGI config for FryedSauces project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'RubyMoonProjectMain.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'FryedSauces.settings')
 
 application = get_wsgi_application()
